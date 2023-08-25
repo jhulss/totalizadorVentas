4d63a9ea4174e3503baa819730c8c0027e17f0d7
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-08-25 19:05:44 -0400

    Tercera prueba, se muestra el total (sin impuesto) segun la cantidad y precio

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -2,13 +2,12 @@ import totalizadorVentas from "./sumador.js";
 
 
 describe("Totalizar", () => {
-  it("deberia mostrar la cantidad de items ingresados", () => {
+  it("deberia mostrar el total en base a la cantidad y el precio de los items", () => {
     const calcu = new totalizadorVentas();
-    expect(calcu.calcularTotal(3)).toEqual(3);
+    expect(calcu.calcularTotal(4, 4)).toEqual(16);
   });
 
-  it("deberia mostrar el precio de los items", () => {
-    const calcu = new totalizadorVentas();
-    expect(calcu.calcularTotal(4)).toEqual(4);
-  });
+  
+
+
 });
