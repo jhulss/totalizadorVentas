d4037351097e4e25ac64d4c9bc1dc72a8377a125
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-08-26 11:58:50 -0400

    Se muestra el porcentaje de descuento segun el codigo del estado ingresado

--- src/presenter.js
+++ src/presenter.js
@@ -2,14 +2,17 @@ import totalizadorVentas from "./sumador";
 
 const first = document.querySelector("#cantidad");
 const second = document.querySelector("#precio");
+const tird = document.querySelector("#estado");
 const form = document.querySelector("#total-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const calcu = new totalizadorVentas();
+  const calcu2 = new totalizadorVentas();
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
+  
 
-  div.innerHTML = "<p>" + calcu.calcularTotal(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + "Total: " + calcu.calcularTotal(firstNumber, secondNumber) + "<br>" +  "Impuesto de estado: " + calcu2.verificarEstado(tird.value) + "</p>";
 });
--- src/sumador.js
+++ src/sumador.js
@@ -6,6 +6,19 @@ class totalizadorVentas
       return cantidad * precio;
     }
 
+    verificarEstado(codigo)
+    {
+      const cod = codigo.toUpperCase();
+      const estados = 
+      {
+        UT: 6.65,
+        NV: 8.00,
+        TX: 6.25,
+        AL: 4.00,
+        CA: 8.25
+      }
+      return estados[cod];
+    }
 
 }
 
